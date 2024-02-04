ec8553ef9a92fd469ac7df27ed763854d2234f08
elduspogi <126318247+elduspogi@users.noreply.github.com>
2024-02-04 15:36:40 +0800

    Delete IT-Elective-3 directory

--- IT-Elective-3/ToDoList/todoapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class ToDoSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ToDo
-        fields = ('id', 'Title', 'Description', 'Date', 'Completed')
--- IT-Elective-3/ToDoList/todoapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-    path('todo-list/<int:pk>/', DetailTodo.as_view()),
-    path('todo-list/', ListTodo.as_view()),
-    path('todo-list/create/', CreateTodo.as_view()),
-    path('todo-list/delete/<int:pk>/', DeleteTodo.as_view()),
-]
